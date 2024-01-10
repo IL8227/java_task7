6094b0928d71a11e563e0f81d1f50e65a6012665
IL8227 <iliaspb2012@gmail.com>
2024-01-10 17:29:55 +0300

    корекция

---
+++ Main.py
@@ -0,0 +1,6 @@
+from Controller import Controller
+from View import View
+
+v = View()
+c = Controller(v)
+c.start()
---
+++ Model/CalcModel.py
@@ -0,0 +1,6 @@
+from Model import MultModel
+
+
+class CalcModel(MultModel):  # Абстрактный класс калькулятора, устанавливает, что у калькулятора два числа
+    x = 0
+    y = 0
